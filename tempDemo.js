22f42e3305e447fd907a972daa5ce97bec3fe10c
Zhou <you@3042391587@qq.com>
2023-03-18 16:25:50 +0800

    create TempDemo

---
+++ tempDemo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world 
+// @namespace    3042391587@qq.com
+// @version      0.1
+// @description  excise Hello world
+// @author       Zhou
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+        console.log("Hello World");
+    // Your code here...
+})();

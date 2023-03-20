b1e57b61fe86a17be0c963cd723c307a8a77f7a9
Zhou <you@3042391587@qq.com>
2023-03-20 13:18:42 +0800

    retry

--- tempDemo.js
+++ tempDemo.js
@@ -13,11 +13,8 @@
 (function() {
     'use strict';
         console.log("change version");
-<<<<<<< HEAD
         console.log("hello world");
         alert("hello world")
-=======
->>>>>>> aace9992622c3f357f33a53ebcda9a924c7734c5
         alert("add alert")
     // Your code here...
 })();
